03f3e04b70ab651700d75ed8f6307a7ae1b790cc
Ben Rew <rewbens@googlemail.com>
2023-04-18 12:44:10 +0100

    updated game status.

--- vanilla build/js/app.js
+++ vanilla build/js/app.js
@@ -6,10 +6,49 @@ const App = {
     resetBtn: document.querySelector('[data-id="reset-btn"]'),
     newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
     squares: document.querySelectorAll('[data-id="square"]'),
+    modal: document.querySelector('[data-id="modal"]'),
+    modalText: document.querySelector('[data-id="modal-text"]'),
+    modalBtn: document.querySelector('[data-id="modal-btn"]'),
+    turn: document.querySelector('[data-id="turn"]'),
   },
 
   state: {
     moves: [],
+  },
+
+  getGameStatus(moves) {
+    const p1Moves = moves
+      .filter((move) => move.playerId === 1)
+      .map((move) => +move.squareId);
+    const p2Moves = moves
+      .filter((move) => move.playerId === 2)
+      .map((move) => +move.squareId);
+
+    const winningPatterns = [
+      [1, 2, 3],
+      [1, 5, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 5, 7],
+      [3, 6, 9],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+
+    let winner = null;
+
+    winningPatterns.forEach((pattern) => {
+      const p1Wins = pattern.every((v) => p1Moves.includes(v));
+      const p2Wins = pattern.every((v) => p2Moves.includes(v));
+
+      if (p1Wins) winner = 1;
+      if (p2Wins) winner = 2;
+    });
+
+    return {
+      status: moves.length === 9 || winner != null ? "complete" : "in-progress", // in-progress | complete
+      winner, //1 | 2 | null
+    };
   },
 
   init() {
@@ -32,6 +71,12 @@ const App = {
       console.log("Add a new round");
     });
 
+    App.$.modalBtn.addEventListener("click", (event) => {
+      App.state.moves = [];
+      App.$.squares.forEach((square) => square.replaceChildren());
+      App.$.modal.classList.add("hidden");
+    });
+
     //TODO
     App.$.squares.forEach((square) => {
       square.addEventListener("click", (event) => {
@@ -42,8 +87,8 @@ const App = {
           );
           return existingMove !== undefined;
         };
-
-        if (hasMove(+square.Id)) {
+        //prevents play twice...exits
+        if (hasMove(+square.id)) {
           return;
         }
 
@@ -55,33 +100,46 @@ const App = {
             ? 1
             : getOppositePlayer(lastMove.playerId);
 
-        const icon = document.createElement("i");
+        const nextPlayer = getOppositePlayer(currentPlayer);
+
+        const squareIcon = document.createElement("i");
+        const turnIcon = document.createElement("i");
+        const turnLabel = document.createElement("p");
+        turnLabel.innerText = `Player ${nextPlayer}, your turn!`;
 
         if (currentPlayer === 1) {
-          icon.classList.add("fa-solid", "fa-x", "yellow");
+          squareIcon.classList.add("fa-solid", "fa-x", "yellow");
+          turnIcon.classList.add("fa-solid", "fa-o", "turquoise");
         } else {
-          icon.classList.add("fa-solid", "fa-o", "turquoise");
+          squareIcon.classList.add("fa-solid", "fa-o", "turquoise");
+          turnIcon.classList.add("fa-solid", "fa-x", "yellow");
         }
 
+        App.$.turn.replaceChildren(turnIcon, turnLabel);
+        
+        //checks history and changes state
         App.state.moves.push({
           squareId: +square.id,
           playerId: currentPlayer,
         });
 
-        square.replaceChildren(icon);
+        square.replaceChildren(squareIcon);
 
         //check if there is a winner or tie game
 
-        const winningPatterns = [
-          [1, 2, 3],
-          [1, 5, 9],
-          [1, 4, 7],
-          [2, 5, 8],
-          [3, 5, 7],
-          [3, 6, 9],
-          [4, 5, 6],
-          [7, 8, 9],
-        ];
+        const game = App.getGameStatus(App.state.moves);
+
+        if (game.status === "complete") {
+          App.$.modal.classList.remove("hidden");
+
+          if (game.winner) {
+            const message = `Player ${game.winner} wins!`;
+          } else {
+            message = "Draw!";
+          }
+
+          App.$.modalText.textContent = message;
+        }
       });
     });
   },
